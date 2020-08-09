914119f588267476faf90a64a9a79cddfe509aac
HaodongWang <m15397117726@163.com>
2020-08-09 15:11:25 +0800

    20200809 class blocks in java

--- learnjava/src/main/java/com/javahdw/learnjava/JustForTest/JustForTest.java
+++ learnjava/src/main/java/com/javahdw/learnjava/JustForTest/JustForTest.java
@@ -1,4 +1,27 @@
 package com.javahdw.learnjava.JustForTest;
 
 public class JustForTest {
+    static{
+        System.out.println("静态代码块");
+    }
+    {
+        System.out.println("构造代码块1");
+    }
+    {
+        System.out.println("构造代码块2");
+    }
+    JustForTest(){
+        System.out.println("构造函数1");
+    }
+    JustForTest(int a){
+        System.out.println("构造函数2");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        JustForTest test = new JustForTest();
+        JustForTest test2 = new JustForTest(1);
+        System.out.println("1");
+
+    }
 }
